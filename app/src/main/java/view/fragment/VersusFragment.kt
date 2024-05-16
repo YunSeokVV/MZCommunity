4aa64d8e767949212934ee15045c39d42716505e
yun_seok <agaroseson@naver.com>
2024-05-16 15:55:02 +0900

    Fix : 일상 게시글 새로고침 버그 수정 - 새로고침후 0번째 포지션으로 가지지 않던 버그 수정

--- app/src/main/java/view/fragment/VersusFragment.kt
+++ app/src/main/java/view/fragment/VersusFragment.kt
@@ -2,14 +2,13 @@ package view.fragment
 
 import android.app.AlertDialog
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
